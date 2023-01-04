751c540631e00d0147416c7840f2b3c50c3a31c2
octopus <lealsamilysuga@gmail.com>
2023-01-04 10:44:53 -0300

    initial commit

---
+++ Execucao.R
@@ -0,0 +1,11 @@
+library(tabuSearch)
+
+z = tabuSearch(size = 12, iters = 100, objFunc = compras,
+               listSize = 9, nRestarts = 10, repeatAll = 1, verbose = T)
+
+plot(z)
+
+summary(z)
+
+z$configKeep[299,]
+summary(z, verbose = TRUE)
